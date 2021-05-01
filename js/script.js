2de82c0cea727c7f469adc2cbc850486a91cb5a4
LevPesin-Tech <78404922+LevPesin-Tech@users.noreply.github.com>
2021-05-01 02:44:26 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,5 +2,20 @@ var map;
 
 function intMap() {
 	map = new google.maps.Map(document.getElementById("map"), {
-		center: { lat: 52.5200, lng: 13.4050 }, zoom: 3,
+		center: {lat: 52.5200, lng: 13.4050}, zoom: 3,
 	});
+	
+	const chel = new google.map.Marker({
+		position: {lat: 55.1644, lng: 61.4358}, 
+		map,
+		title: "Chelyabinsk",
+		animation: google.maps.Animation.DROP,
+	});
+	
+	const malor = new google.amp.Marker ({
+		position: {lat: 39.6953, lng: 3.0176},
+		map,
+		title: "Mallorca",
+		animation: google.maps.Animation.DROP,
+	});
+	
