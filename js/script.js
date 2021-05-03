c7f27591116fc11a9d61e17da6915d532041f5c5
LevPesin-Tech <78404922+LevPesin-Tech@users.noreply.github.com>
2021-05-03 02:16:57 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: { lat: 52.5200, lng: 13.4050 },
+    zoom: 3,
+    center: {lat: 52.5200, lng: 13.4050},
   });
   const chel = new google.maps.Marker({
-    position: { lat: 55.1644, lng: 61.4358 },
+    position: {lat: 55.1644, lng: 61.4358},
     map,
     title: "Chelyabinsk",
     animation: google.maps.Animation.DROP,
   });
   const malor = new google.maps.Marker({
-    position: { lat: 39.6953, lng: 3.0176 },
+    position: {lat: 39.6953, lng: 3.0176},
     map,
     title: "Mallorca",
     animation: google.maps.Animation.DROP,
   });
   const cali = new google.maps.Marker({
-    position: { lat: 44.7229, lng: -117.9298 },
+    position: {lat: 44.7229, lng: -117.9298},
     map,
     title: "Newport Beach",
     animation: google.maps.Animation.DROP,
   });
   const abha = new google.maps.Marker({
-    position: { lat: 43.2795, lng: 40.2706 },
+    position: {lat: 43.2795, lng: 40.2706},
     map,
     title: "Gagra, Abkhazia",
     animation: google.maps.Animation.DROP,
