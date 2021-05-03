580df0fd4de27f58d4e77d268c742297a5904cc1
LevPesin-Tech <78404922+LevPesin-Tech@users.noreply.github.com>
2021-05-03 01:16:22 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,47 +1,45 @@
-var map;
-
-function initMap() {
-	const map = new google.maps.Map(document.getElementById("map"), {
-		center: {lat: 52.5200, lng: 13.4050}, 
+function initMap(){
+var el = document.getElementById('map');
+	var myLocation = new google.maps.LatLng(52.5200, 13.4050);
+	var mapOptions = {
+		center: myLocation,
 		zoom: 2,
-	});
+		mapTypeId: google.maps.MapTypeId.hybrid,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
+		}
+	};
+
+    var myMap = new google.maps.Map(el, mapOptions);
 	
-	const chel = new google.map.Marker({
-		position: {lat: 55.1644, lng: 61.4358}, 
-		map,
-		title: "Chelyabinsk",
-		animation: google.maps.Animation.DROP,
-	});
-	var myMap = new google.maps.Map(el, mapOptions);
 	
-	var marker = new google.maps.Marker({
-      position: {lat: 40, lng: 20},
+	var chel = new google.maps.Marker({
+      position: {lat: 55.1644, lng: 61.4358},
       map: myMap,
-      animation: google.maps.Animation.DROP,
-      title: "Test"
-      
-  });
+	  title: "Chelyabinsk",
+      animation: google.maps.Animation.DROP
+    });
 	
-	const malor = new google.map.Marker({
-		position: {lat: 39.6953, lng: 3.0176},
-		map: map,
-		title: "Mallorca",
-		animation: google.maps.Animation.DROP,
-	});
+	var malor = new google.maps.Marker({
+      position: {lat: 39.6953, lng: 3.0176},
+      map: myMap,
+	  title: "Mallorca",
+      animation: google.maps.Animation.DROP
+    });
 	
-	const cali = new google.map.Marker({
-		position: {lat: 33.6189, lng: -117.9298},
-		map,
-		title: "Newport Beach",
-		animation: google.maps.Animation.DROP,
-	});
+	var cali = new google.maps.Marker({
+      position: {lat: 33.6189, lng: -117.9298},
+      map: myMap,
+	  title: "Newport Beach",
+      animation: google.maps.Animation.DROP
+    });
 	
-	const abha = new google.map.Marker({
-		position: {lat: 43.2795, lng: 40.2706},
-		map,
-		title: "Gagra, Abkhazia",
-		animation: google.maps.Animation.DROP,
-	});
+	var abha = new google.maps.Marker({
+      position: {lat: 43.2795, lng: 40.2706},
+      map: myMap,
+	  title: "Gagra, Abkhazia",
+      animation: google.maps.Animation.DROP
+    });
 }
 google.maps.event.addDomListener(window, 'load', initMap);
 		
