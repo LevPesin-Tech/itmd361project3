72f5ef9eccca746b5baa14dd5c9c4ee0f585646e
LevPesin-Tech <78404922+LevPesin-Tech@users.noreply.github.com>
2021-05-03 04:36:48 -0500

    slide

--- js/sliderM.js
+++ js/sliderM.js
@@ -1,7 +1,5 @@
-$(function(){
-  $('.bxslider').bxSlider({
-    mode: 'fade',
-    captions: true,
-    slideWidth: 600
+$(document).ready(function(){
+    $('.slider').bxSlider({
+      captions: true
+    });
   });
-});
