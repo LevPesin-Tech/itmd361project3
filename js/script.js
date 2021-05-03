96fae37b59d3645ae02156bba68acfa9a7cb29a3
LevPesin-Tech <78404922+LevPesin-Tech@users.noreply.github.com>
2021-05-03 01:25:30 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,47 +1,80 @@
-function initMap(){
-var el = document.getElementById('map');
-alert('it works');
-	var myLocation = new google.maps.LatLng(52.5200, 13.4050);
-	var mapOptions = {
-		center: myLocation,
-		zoom: 6,
-		mapTypeId: google.maps.MapTypeId.roadmap,
-		mapTypeControlOptions: {
-			position: google.maps.ControlPosition.BOTTOM_CENTER
-		}
-	};
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: { lat: 39.6759, lng: -101.9390 },
+  });
+  const home = new google.maps.Marker({
+    position: { lat: 41.8906, lng: -87.6399 },
+    map,
+    title: "Chicago",
+    icon: "images/home.png",
+    animation: google.maps.Animation.DROP,
+  });
+  const wyomingVisit = new google.maps.Marker({
+    position: { lat: 42.9543, lng: -107.6443 },
+    map,
+    title: "Wyoming",
+    icon: "images/yellowstone.png",
+    animation: google.maps.Animation.DROP,
+  });
+  const michiganVisit = new google.maps.Marker({
+    position: { lat: 43.7871, lng: -84.7195 },
+    map,
+    title: "Michigan",
+    icon: "images/camp.png",
+    animation: google.maps.Animation.DROP,
+  });
+  const wisconsinVisit = new google.maps.Marker({
+    position: { lat: 44.7229, lng: -89.6151 },
+    map,
+    title: "Wisconsin",
+    icon: "images/waterpark.png",
+    animation: google.maps.Animation.DROP,
+  });
+  const floridaVisit = new google.maps.Marker({
+    position: { lat: 27.7169, lng: -81.6192 },
+    map,
+    title: "Florida",
+    icon: "images/beach.png",
+    animation: google.maps.Animation.DROP,
+  });
+  const arizonaVisit = new google.maps.Marker({
+    position: { lat: 34.4706, lng: -111.7550 },
+    map,
+    title: "Arizona",
+    icon: "images/grandcanyon.png",
+    animation: google.maps.Animation.DROP,
+  });
 
-    var myMap = new google.maps.Map(el, mapOptions);
-	
-	
-	var chel = new google.maps.Marker({
-      position: {lat: 55.1644, lng: 61.4358},
-      map: myMap,
-      animation: google.maps.Animation.DROP,
-	  title: "Chelyabinsk"
-    });
-	
-	var malor = new google.maps.Marker({
-      position: {lat: 39.6953, lng: 3.0176},
-      map: myMap,
-	  title: "Mallorca",
-      animation: google.maps.Animation.DROP
-    });
-	
-	var cali = new google.maps.Marker({
-      position: {lat: 33.6189, lng: -117.9298},
-      map: myMap,
-	  title: "Newport Beach",
-      animation: google.maps.Animation.DROP
-    });
-	
-	var abha = new google.maps.Marker({
-      position: {lat: 43.2795, lng: 40.2706},
-      map: myMap,
-	  title: "Gagra, Abkhazia",
-      animation: google.maps.Animation.DROP
-    });
+  const wyomingInfo = new google.maps.InfoWindow({
+    content: '<div class="mapinfo"><h2>Wyoming</h2><h4>Visited Yellowstone National Park</h4><p>Drove by car</p></div>',
+  });
+  const michiganInfo = new google.maps.InfoWindow({
+    content: '<div class="mapinfo"><h2>Michigan</h2><h4>Went camping</h4><p>Drove by car</p></div>',
+  });
+  const wisconsinInfo = new google.maps.InfoWindow({
+    content: '<div class="mapinfo"><h2>Wisconsin</h2><h4>Visited a Waterpark</h4><p>Drove by car</p></div>',
+  });
+  const floridaInfo = new google.maps.InfoWindow({
+    content: '<div class="mapinfo"><h2>Florida</h2><h4>Visited the beach</h4><p>Flew by Plane</p></div>',
+  });
+  const arizonaInfo = new google.maps.InfoWindow({
+    content: '<div class="mapinfo"><h2>Arizona</h2><h4>Visited Grand Canyon National Park</h4><p>Flew by Plane</p></div>',
+  });
+
+  wyomingVisit.addListener("click", () => {
+    wyomingInfo.open(map, wyomingVisit);
+  });
+  michiganVisit.addListener("click", () => {
+    michiganInfo.open(map, michiganVisit);
+  });
+  wisconsinVisit.addListener("click", () => {
+    wisconsinInfo.open(map, wisconsinVisit);
+  });
+  floridaVisit.addListener("click", () => {
+    floridaInfo.open(map, floridaVisit);
+  });
+  arizonaVisit.addListener("click", () => {
+    arizonaInfo.open(map, arizonaVisit);
+  });
 }
-google.maps.event.addDomListener(window, 'load', initMap);
-		
-	
